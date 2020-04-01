4ea2710ee39aa9f042c9fb93c5b99c158a32ddb4
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-04-01 18:40:56 -0400

    Fixing linter issues

--- single-number/single-number-hash.py
+++ single-number/single-number-hash.py
@@ -1,42 +1,13 @@
-if __name__ == '__main__':
-    main()
-
-    class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+class Solution:
+    def singleNumber(self, nums):
         count_map = {}
-        
+
         for num in nums:
             if(num in count_map):
                 count_map[num] += 1
             else:
                 count_map[num] = 1
-        
+
         for key in count_map:
             if(count_map[key] == 1):
                 return key
-
-def stringToIntegerList(input):
-    return json.loads(input)
-
-def main():
-    import sys
-    import io
-    def readlines():
-        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
-            yield line.strip('\n')
-
-    lines = readlines()
-    while True:
-        try:
-            line = next(lines)
-            nums = stringToIntegerList(line);
-            
-            ret = Solution().singleNumber(nums)
-
-            out = str(ret);
-            print(out)
-        except StopIteration:
-            break
-
-if __name__ == '__main__':
-    main()
--- single-number/single-number-xor.py
+++ single-number/single-number-xor.py
@@ -1,35 +1,7 @@
-import sys
-
-
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber(self, nums):
         current = 0
         for num in nums:
             current ^= num
 
         return current
-
-
-def stringToIntegerList(input):
-    return json.loads(input)
-
-
-def main():
-  import io
-
-   def readlines():
-        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
-            yield line.strip('\n')
-
-    lines = readlines()
-    while True:
-        try:
-            line = next(lines)
-            nums = stringToIntegerList(line)
-
-            ret = Solution().singleNumber(nums)
-
-            out = str(ret)
-            print(out)
-        except StopIteration:
-            break
