a8091067a4752e653789905d6168fd4887b83296
Eleazar Maestre <eleazarenrique23@gmail.com>
2020-04-13 22:43:10 -0400

    Contiguos Array solution in Python

---
+++ contiguous-array/contiguous-array.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Solution:
+    def findMaxLength(self, nums) -> int:
+        dir = {}
+        dir[0] = -1
+        maxlen = 0
+        count = 0
+        numsLen = len(nums)
+        
+        for i in range(numsLen):
+            count = count + (1 if nums[i] == 1 else -1)
+            if (count in dir):
+                maxlen = max(maxlen, i - dir[count])
+            else:
+                dir[count] = i
+            
+        return maxlen;
